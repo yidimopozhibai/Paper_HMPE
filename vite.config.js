77d37c60f952a7edc94760d8021a680b3744dca4
yidimopozhibai <81897860+yidimopozhibai@users.noreply.github.com>
2025-04-05 12:30:46 +0800

    Update vite.config.js
    Paper_HMPE

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+  base:'/Paper_HMPE',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
